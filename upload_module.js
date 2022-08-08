f7cbbdde50be759a9b66c3bc990520cdf7eb6387
musaejaz1 <musa.ejaz1@outlook.com>
2022-08-09 02:25:13 +0500

    uploaded form input for balance brackets test

--- upload_module.js
+++ upload_module.js
@@ -1,12 +1,30 @@
 var http = require('http');
 var formidable = require('formidable');
 var fs = require('fs');
+var dt_obj = require('./myfirstmodule');
 
 http.createServer(function (req, res) {
     if (req.url == '/fileupload') {
         var form = new formidable.IncomingForm();
         form.parse(req, function (err, fields, files) {
-            res.write(JSON.stringify(fields));
+            var field1 = fields.field1;
+            var field2 = fields.field2;
+
+            field1 = Array.from(field1);
+            field2 = Array.from(field2);
+            if(dt_obj.myDateTime(field1)>0){
+                res.write("Field 1 is false");
+            }
+            else{
+                res.write('Field 1 is true');
+            }
+            res.write('<br>');
+            if(dt_obj.myDateTime(field2)>0){
+                res.write("Field 2 is false");
+            }
+            else{
+                res.write('Field 2 is true');
+            }
             res.end();
         });
     } else {
